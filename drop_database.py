51a16e2f646beab1fcff70534d913762dbd17770
alexey-samarev <Lesha200585@gmail.com>
2024-04-03 21:06:40 +0300

    1

---
+++ drop_database.py
@@ -0,0 +1,23 @@
+"""Модуль для удаления данных и таблиц из бд."""
+
+from migrate import execute_command
+
+TABLES = (
+    "product",
+    "ordert",
+    "rack",
+    "rack_product_link",
+    "order_product_link",
+)
+
+
+def drop() -> None:
+    """Основная функция мода."""
+    for table in TABLES:
+        sql_command = f"DROP TABLE {table}"
+        execute_command(sql_command)
+
+
+if __name__ == "__main__":
+    drop()
+    
