c534996929bc8e0b38fd86ada010afd21ef11301
alexey-samarev <Lesha200585@gmail.com>
2024-04-03 20:54:14 +0300

    1

--- load_test_data.py
+++ load_test_data.py
@@ -1,34 +1,63 @@
-"""Модуль для загрузки тестовых данных в бд"""
+"""Модуль для загрузки тестовых данных в бд."""
 
-from migrate import migrate, execute_command
 from database import Database
+from migrate import migrate
+from settings import settings
 
 
-def execute_many_command(sql_command: str, data: list[tuple[str | int]],
+def execute_many_command(
+    sql_command: str,
+    data: list[tuple[str | int]],
 ) -> None:
-    """Выполнить команду с различными данными.
-    
-    Args:
-    
-        sql-command (str): сама sql команда.
-        data (list[tuple])
-    """
+    """Выполнить команду c  разлиными данными.
 
-db = Database(settings=settings)
-with db() as conn:
-    cur = conn.cursor()
-    cur.executemany(sql_command, data)
-    conn.commit()
+    Args:
+    ----
+        sql_command (str): сама sql команда.
+        data (list[tuple[str  |  int]]): данные в нужном формате
+
+    """
+    db = Database(settings=settings)
+    with db() as conn:
+        cur = conn.cursor()
+        cur.executemany(sql_command, data)
+        conn.commit()
 
 
 rack_insert = """
 INSERT INTO rack(name)
-VALUES(%s)
+VALUES (%s)
 """
-rack_insert_values = [("А"), ("Б"), ("Ж")]
+rack_insert_values = [("А",), ("Б",), ("Ж",)]
+
+order_insert = """
+INSERT INTO ordert(order_number, consumer_name)
+VALUES (%s, %s)
+"""
+order_insert_values = [(10, "A"), (11, "B"), (14, "C"), (15, "D")]
+
+product_insert = """
+INSERT INTO product(name, article)
+VALUES (%s, %s)
+"""
+product_insert_values = [
+    ("Ноутбук", 1),
+    ("Телевизор", 2),
+    ("Телефон", 3),
+    ("Системный блок", 4),
+    ("Часы", 5),
+    ("Микрофон", 6),
+]
+
+
 def load() -> None:
     """Основная функция модуля."""
     migrate()
     execute_many_command(rack_insert, rack_insert_values)
+    execute_many_command(order_insert, order_insert_values)
+    execute_many_command(product_insert, product_insert_values)
 
-if __name__  == "__main__"
+
+if __name__ == "__main__":
+    load()
+    
--- migrate.py
+++ migrate.py
@@ -65,7 +65,7 @@ CONSTRAINT FK_link_product_order FOREIGN KEY(product_id)
 REFERENCES product(id)
 )
 """
-def main() - None:
+def main() -> None:
     """Основная функция модуля."""
 execute_command(product_table_create)
 execute_command(rack_table_create)
@@ -75,4 +75,3 @@ execute_command(rack_product_link_create)
 
 if __name__ == "__main__":
     migrate()
-    
